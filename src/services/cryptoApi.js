0a48f87f68ad9c261277f87efa0ba6fc5db239ca
Daniel <danielchoe7@gmail.com>
2021-12-09 11:45:03 +1100

    env file

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,10 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
   'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-  'x-rapidapi-key': '07ff6f15e2msh6c5ceba69640d44p1d8732jsn31c4ae083941'
+  'x-rapidapi-key': `${process.env.REACT_APP_API_KEY}`
 }
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
-
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders});
 
